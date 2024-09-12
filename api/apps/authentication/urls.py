b43949aee18dacc5fa0edcc6cb63f024e3a3ccf0
Fernando Leite <fernandogfleite@gmail.com>
2024-09-12 01:47:08 -0300

    Refactor authentication urls.py to add RetriveMeView

--- api/apps/authentication/urls.py
+++ api/apps/authentication/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from api.apps.authentication.serializers.jwt import MyTokenObtainPairSerializer
+from api.apps.authentication.views.authentication import RetriveMeView
 
 app_name = "authentication"
 
@@ -12,4 +13,5 @@ urlpatterns = [
         name="token_obtain_pair",
     ),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("me/", RetriveMeView.as_view(), name="me"),
 ]
