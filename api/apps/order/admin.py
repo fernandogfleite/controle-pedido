6a3a350aeb3618b7488a5a39b754ee241a522318
Fernando Leite <fernandogfleite@gmail.com>
2024-09-12 01:45:58 -0300

    Refactor admin.py to register Table model in order app

--- api/apps/order/admin.py
+++ api/apps/order/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from api.apps.order.models.order import Table
+
+
 # Register your models here.
+class TableAdmin(admin.ModelAdmin):
+    list_display = ("number", "status", "description")
+    list_filter = ("status",)
+    search_fields = ("number", "description")
+
+
+admin.site.register(Table, TableAdmin)
